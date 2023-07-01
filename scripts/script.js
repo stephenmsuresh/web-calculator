158badfe248ec9ef3add2538b9d81b396497439f
Stephen Suresh <stephen.suresh@outlook.com>
2023-07-01 10:59:11 -0400

    add keyboard support

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,10 @@
-//to do: format output
+//todo: 
+//add keyboard support
+//js30 video on drumkit
+
+//future improvements:
+//exponent button
+
 const MAX_OUTPUT_LENGTH = 15;
 
 const inputScreen = document.querySelector('#input-screen');
@@ -21,7 +27,7 @@ operators.forEach((operator) => {
 
 document.querySelector('#equals').addEventListener('click', (evt) => {
     if (operator != '') {
-        calculate(operator);
+        calculate();
     }
     else if ((inputScreen.innerText) != "0") {
         outputScreen.textContent = formatOutput(inputScreen.textContent);
@@ -30,6 +36,7 @@ document.querySelector('#equals').addEventListener('click', (evt) => {
 })
 
 function inputNumber(evt) {
+    //if inputScreen is 'empty' ie showing 0 at the start like a calculator
     if ((inputScreen.textContent) == "0") {
         inputScreen.textContent = "";
     }
@@ -37,7 +44,8 @@ function inputNumber(evt) {
         outputScreen.textContent = "";
     }
     if (inputScreen.textContent.length < 20) {
-        inputScreen.textContent += evt.target.innerText;
+        let num = (evt.type === 'click' ? (evt.target.innerText) : (evt.key))
+        inputScreen.textContent += num;
     }
 }
 
@@ -48,16 +56,15 @@ function handleOperator(evt) {
     else if (outputScreen.textContent === "") {
         outputScreen.textContent = formatOutput(inputScreen.textContent);
     }
-    else {
-
-    }
-    operator = evt.target.id;
+    // else {
+    // }
+    operator = ((evt.type === 'click') ? evt.target.id : convertOperator(evt.key));
     inputScreen.textContent = "0";
 }
 
-function calculate(mathOperation) {
+function calculate() {
     let result;
-    switch (mathOperation) {
+    switch (operator) {
         case "divide":
             result = ((+outputScreen.textContent) / (+inputScreen.textContent));
             break;
@@ -70,6 +77,8 @@ function calculate(mathOperation) {
         case "add":
             result = ((+outputScreen.textContent) + (+inputScreen.textContent));
             break;
+        default:
+            result = +(inputScreen.textContent);
     }
     if (result === NaN || !isFinite(result)) {
         outputScreen.textContent = "ERROR";
@@ -105,18 +114,20 @@ function decimalPoint() {
 }
 
 document.querySelector('#backspace').addEventListener('click', (evt) => {
+    backspace();
+})
+
+function backspace() {
     if (inputScreen.textContent != "0") {
         inputScreen.textContent = inputScreen.textContent.slice(0, -1);
         if (inputScreen.textContent.length === 0) {
             inputScreen.textContent = "0";
         }
     }
-})
+}
 
 function formatOutput(output) {
     let num = output.toString();
-    console.log(num);
-    console.log(num.length)
     if (num.length > MAX_OUTPUT_LENGTH) {
         num = (exponent(output).toString());
         while (num.length > MAX_OUTPUT_LENGTH) {
@@ -128,4 +139,51 @@ function formatOutput(output) {
 
 function exponent(x, f) {
     return Number.parseFloat(x).toExponential(f);
+}
+
+//keyboard support
+window.addEventListener('keydown', (evt) => {
+    if (isNumber(evt.key)) {
+        inputNumber(evt);
+    }
+    if (isOperator(evt.key)) {
+        handleOperator(evt);
+    }
+    if (isEnter(evt.key)) {
+        calculate();
+    }
+    if (evt.key === '.') {
+        decimal();
+    }
+    if (evt.key === 'Backspace') {
+        backspace();
+    }
+});
+
+function isNumber(num) {
+    // return (parseInt(num) == NaN);
+    return (!(isNaN(parseInt(num))));
+}
+
+function isOperator(op) {
+    return (op === '+' ||
+        op === '-' ||
+        op === '/' ||
+        op === '*');
+}
+function isEnter(keyPress) {
+    return (keyPress === "Enter");
+}
+
+function convertOperator(op) {
+    switch (op) {
+        case '+':
+            return "add";
+        case '-':
+            return "subtract";
+        case '/':
+            return "divide";
+        case '*':
+            return "multiply";
+    }
 }
